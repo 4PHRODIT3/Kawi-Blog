d87152352341788a3be2cb4a7352d785f6c94507
4PHRODIT3 <b8_htetmyatlinn@uit.edu.mm>
2022-02-22 23:33:26 +0630

    Fixed Unresponsiveness and Added Last 3 Articles Engagement Chart

--- view/panel.view.php
+++ view/panel.view.php
@@ -12,8 +12,9 @@
     
     
     $visitors_counts = countFromDateArray($viewers, "date");
-    $chart2_titles = [$recent_articles[0]['title'],$recent_articles[1]['title'],$recent_articles[2]['title']];
-    $chart2_values = [$recent_articles[0]['total_viewers'],$recent_articles[1]['total_viewers'],$recent_articles[2]['total_viewers']];
+  
+    $chart2_titles = [compressText($recent_articles[0]['title'], 10),compressText($recent_articles[1]['title'], 10),compressText($recent_articles[2]['title'], 10)];
+    $chart2_values = trimArrayValueWithKey($recent_articles, "total_viewers");
     
 ?>
 
@@ -21,16 +22,16 @@
         <div class="col-12">
             <div class="card my-3 min-vh-100 mb-3 shadow-lg pt-4 px-3 p-lg-5 rounded">
                 <div class="row">
-                    <div class="col-12 col-xl-6 col-xl-5">
+                    <div class="col-12 col-xl-6 col-xl-5 mb-5 mb-xl-0">
                         <h4>Daily Visitors</h4>
-                        <div class="my-3 my-xl-0 chart-container" >
+                        <div class="my-3 my-xl-0 chart-container mb-xl-0" >
                             <canvas id="10days-visitors-chart" class="w-100 h-100"></canvas>
                         </div>
                         
                     </div>
-                    <div class="col-12 col-xl-6 col-xl-5 mt-4 mt-xl-0">
+                    <div class="col-12 col-xl-6 col-xl-5 my-5 my-xl-0">
                         <h4>Recent Articles Engagement</h4>
-                        <div class="my-3 my-xl-0 chart-container">
+                        <div class="my-3 my-xl-0 chart-container pb-5 pb-xl-0">
                             <canvas id="engagement-chart" class="w-100 h-100"></canvas>
                         </div>
                         
@@ -82,17 +83,31 @@ const myChart = new Chart(ctx, {
             }
         },
         responsive: true,
-        maintainAspectRatio:true
+        maintainAspectRatio:true,
+        plugins: {
+            legend: {
+                labels: {
+                    // This more specific font property overrides the global property
+                    font: {
+                        size: 16,
+                        family: "'Noto Sans Myanmar','Roboto',sans-serif",
+                    }
+                }
+            }
+        }
     }
 });
+
+const splitLabels = <?= json_encode($chart2_titles) ?>;
+
 
 const ctx2 = document.getElementById('engagement-chart').getContext('2d');
 const engagementChart = new Chart(ctx2, {
     type: 'bar',
     data: {
-        labels:['Web3','Web2','Web1.0'],
+        labels:<?= json_encode($chart2_titles) ?>,
         datasets: [{
-            label: '# of Engagement',
+            label: '# Engagement of Last Uploaded Articles',
             data: <?= json_encode($chart2_values) ?>,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -120,7 +135,18 @@ const engagementChart = new Chart(ctx2, {
             }
         },
         responsive: true,
-        maintainAspectRatio:true
+        maintainAspectRatio:true,
+        plugins: {
+            legend: {
+                labels: {
+                    // This more specific font property overrides the global property
+                    font: {
+                        size: 16,
+                        family: "'Noto Sans Myanmar','Roboto',sans-serif",
+                    }
+                }
+            }
+        }
     }
 });
 </script>
